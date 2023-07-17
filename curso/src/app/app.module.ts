a605a8a9d1eccbfef432fde09a859e766699b919
Javier <jmartin@grupoloyal.com>
2023-07-17 15:29:13 +0200

    recovery

---
+++ curso/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
+import { MyCoreModule } from '@my/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule,
+    AppRoutingModule, MainModule, SecurityModule, MyCoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
