e353c39b5f368b00c22f2a5798e867a52da6f023
Javier <jmartin@grupoloyal.com>
2023-07-18 14:28:48 +0200

    refactorizado appComponent

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { LoggerService } from '@my/core';
-import { NotificationService, NotificationType } from './common-services';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +6,4 @@ import { NotificationService, NotificationType } from './common-services';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hola mundo';
-
-  // constructor(private log: LoggerService) {
-  //   log.error('Es un error')
-  //   log.warn('Es un warn')
-  //   log.info('Es un info')
-  //   log.log('Es un log')
-  // }
-
-  // constructor(private notify: NotificationService) {}
-  // ngOnInit(): void {
-  //   this.notify.add('Inicio la aplicación', NotificationType.info)
-  // }
 }
