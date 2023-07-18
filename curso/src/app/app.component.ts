8333c161ea2b54ba055bacfe377bcef5180411bf
Javier <jmartin@grupoloyal.com>
2023-07-18 10:40:05 +0200

    logger

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggerService } from '@my/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hola mundo';
 
+  constructor(private log: LoggerService) {
+    log.error('Es un error')
+    log.warn('Es un warn')
+    log.info('Es un info')
+    log.log('Es un log')
+  }
 }
