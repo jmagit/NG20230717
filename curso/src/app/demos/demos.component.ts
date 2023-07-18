36f60d5922f1478dbf245eb0d98118c1bb3a01be
Javier <jmartin@grupoloyal.com>
2023-07-18 14:52:12 +0200

    clase del componente

--- curso/src/app/demos/demos.component.ts
+++ curso/src/app/demos/demos.component.ts
@@ -9,16 +9,60 @@ import { Unsubscribable } from 'rxjs';
 })
 export class DemosComponent implements OnInit, OnDestroy {
   private suscriptor: Unsubscribable | undefined;
+  private nombre: string = 'mundo';
+  fecha = '2023-07-19'
+  fontSize = 24
+  listado = [
+    {id: 1, nombre: 'Madrid'},
+    {id: 2, nombre: 'BARCELONA'},
+    {id: 3, nombre: 'valencia'},
+    {id: 4, nombre: 'ciudad Real'},
+  ]
+  idProvincia = 2
+
+  resultado?: string;
+  visible = true
+  estetica = { importante: true, urgente: false, error: false}
 
   constructor(public vm: NotificationService) { }
 
+  public get Nombre(): string { return this.nombre }
+  public set Nombre(value: string) {
+    if(value === this.nombre) return
+    this.nombre = value
+  }
+
+  public saluda(): void {
+    this.resultado = `Hola ${this.nombre}`
+  }
+  public despide(): void {
+    this.resultado = `Adios ${this.nombre}`
+  }
+  di(algo: string) {
+    this.resultado = `Dice ${algo}`
+  }
+
+  cambia() {
+    this.visible = !this.visible
+    this.estetica.error = !this.estetica.error
+    this.estetica.importante = !this.estetica.importante
+  }
+
+  calcula(a: number, b: number): number { return a + b }
+
+  add(provincia: string) {
+    const id = this.listado[this.listado.length - 1].id + 1
+    this.listado.push({id, nombre: provincia})
+    this.idProvincia = id
+  }
+
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
-    this.suscriptor = this.vm.Notificacion.subscribe(n => {
-      if (n.Type !== NotificationType.error) { return; }
-      window.alert(`Suscripción: ${n.Message}`);
-      this.vm.remove(this.vm.Listado.length - 1);
-    });
+    // this.suscriptor = this.vm.Notificacion.subscribe(n => {
+    //   if (n.Type !== NotificationType.error) { return; }
+    //   window.alert(`Suscripción: ${n.Message}`);
+    //   this.vm.remove(this.vm.Listado.length - 1);
+    // });
   }
   ngOnDestroy(): void {
     if (this.suscriptor) {
